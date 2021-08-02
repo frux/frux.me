f4b0c4229cf46d4f0de74831d969946a63c518d0
frux <mail@frux.me>
2021-08-03 00:25:56 +0300

    Add Github link

--- components/contacts/index.tsx
+++ components/contacts/index.tsx
@@ -1,6 +1,9 @@
 import { Button } from '$components/button';
 import IconMail from '$components/icons/mail.svg';
+import IconGithub from '$components/icons/github.svg';
 import { profile } from '$data/profile';
+
+import styles from './contacts.module.css';
 
 interface IProps {
 	className?: string;
@@ -10,10 +13,19 @@ export function Contacts(props: IProps) {
 	const { className } = props;
 
 	return (
-		<address className={className}>
-			<Button href={`mailto:${profile.contacts.email}`}>
-				<IconMail />
-			</Button>
+		<address className={`${styles.contacts} ${className}`}>
+			<ul className={styles.links}>
+				<li className={styles.link}>
+					<Button rel="noopener nofollow" href={`mailto:${profile.contacts.email}`}>
+						<IconMail />
+					</Button>
+				</li>
+				<li className={styles.link}>
+					<Button target="_blank" rel="noopener nofollow" href="https://github.com/frux">
+						<IconGithub />
+					</Button>
+				</li>
+			</ul>
 		</address>
 	);
 }
